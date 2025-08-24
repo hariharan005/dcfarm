60b5513a64cff9490c3b242493c2790140d4989f
hariharan005 <harichella005@gmail.com>
2025-08-24 12:46:58 +0530

    All the files and folders for this porject is created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// Import your components here
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+// import MainContent from '../components/MainContent';
+
+const Home = () => {
+    return (
+        <div>
+            {/* Add your components below */}
+            {/* <Header /> */}
+            {/* <MainContent /> */}
+            {/* <Footer /> */}
+            <h1>Welcome to the Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
