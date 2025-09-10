aba508a1cda00fcd15c10f88ec6cb59beab55ce6
hariharan005 <harichella005@gmail.com>
2025-09-10 21:46:43 +0530

    adminpage api changed

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -30,7 +30,7 @@ import LoyaltyPoint from "./Marketing/LoyaltyPoint";
 import Notification from "./Marketing/SendNotification";
 
 // Configure axios to send cookies with requests
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || "https://dcfarm.onrender.com/api"; // Update if backend is hosted elsewhere
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || "https://dcfarm.onrender.com"; // Update if backend is hosted elsewhere
 axios.defaults.withCredentials = true;
 
 const AdminPage = () => {
