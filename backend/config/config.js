a5e65930fad384831b52e8c14fce2750bd751b72
hariharan005 <harichella005@gmail.com>
2025-09-10 11:40:56 +0530

    env updated

--- backend/config/config.js
+++ backend/config/config.js
@@ -9,4 +9,9 @@ module.exports = {
 
   RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
   RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
+
+  FRONTEND_URL: process.env.FRONTEND_URL,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+
 };
